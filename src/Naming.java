48c9cc449084462b0af5d384a41683d8696332da
BryanGaray <bryangarayacademico@gmail.com>
2023-09-18 22:50:26 -0500

    Variables

---
+++ src/Naming.java
@@ -0,0 +1,18 @@
+// UpperCamelCase - Classes
+// The name of the file has to be the same of the main class
+public class Naming {
+    public static void main(String[] args) {
+        // Naming options
+        String $countryName = "Ecuador";
+        String _city = "Guayaquil";
+
+        // CONST
+        int POSITION = 10;
+        int MAX_WIDTH = 200;
+        int MIN_WIDTH = 300;
+
+        // lowerCamelCase
+        String fullName = "Bryan Enrique Garay Benavidez";
+        int sizeInCentimeters = 25;
+    }
+}
