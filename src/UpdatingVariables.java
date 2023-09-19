4257785debc37d3c40659bdefbecb0bd31976834
BryanGaray <bryangarayacademico@gmail.com>
2023-09-18 22:28:02 -0500

    first commit

---
+++ src/UpdatingVariables.java
@@ -0,0 +1,16 @@
+public class UpdatingVariables {
+    public static void main(String[] args) {
+        // Ints
+        int salary = 1000;
+        // Update 1
+        salary = salary + 500;
+        // Update 2
+        salary -= 200;
+        System.out.println(salary);
+
+        // Strings
+        String employeeName = "Bryan Enrique";
+        employeeName = employeeName + " Garay Benavidez";
+        System.out.println("Mi nombre es "+employeeName);
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declarar variable
+        int speed;
+        // Inicializamos la variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        System.out.println(salary);
+
+        String employeeName = "Mana";
+        System.out.println(employeeName);
+    }
+}
